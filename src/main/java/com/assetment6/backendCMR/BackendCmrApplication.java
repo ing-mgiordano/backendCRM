5e7dbc5e4a72a31c10509d40ebd84e62b720ab0d
migiorda <miguel.giordano@solera.com>
2022-11-28 13:06:10 +0100

    init py

---
+++ src/main/java/com/assetment6/backendCMR/BackendCmrApplication.java
@@ -0,0 +1,13 @@
+package com.assetment6.backendCMR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCmrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assetment6/backendCMR/BackendCmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assetment6.backendCMR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
