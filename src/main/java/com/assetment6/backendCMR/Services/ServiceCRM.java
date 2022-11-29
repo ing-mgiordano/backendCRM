497c9287fa6376b4c31d6e03a7a9db469ca3bd5d
migiorda <miguel.giordano@solera.com>
2022-11-29 16:33:56 +0100

    test login green

--- src/main/java/com/assetment6/backendCMR/Services/ServiceCRM.java
+++ src/main/java/com/assetment6/backendCMR/Services/ServiceCRM.java
@@ -1,9 +1,25 @@
 package com.assetment6.backendCMR.Services;
 
+import java.util.ArrayList;
+
+import com.assetment6.backendCMR.Entities.Users;
+
 public class ServiceCRM {
 
+    private ArrayList<Users> user;
+    
+    public ServiceCRM() {
+        this.user = new ArrayList<>();
+        this.user.add(new Users(0, "Miguel", "migiorda", "usuario@solera.com", "bootcamp4"));
+    }
+
     public boolean login(String string, String string2) {
-        return false;
+        boolean login = false;
+
+        for(Users U:this.user) {
+            if(string == U.getEmail() && string2 == U.getPassword()) login = true;
+        }
+        return login;
     }
     
 }
